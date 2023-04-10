490a88e14e41cdd344a1cf7b58f5ce892c01842b
Mashuk Grigoriy <mashukgrisha@mail.ru>
2023-04-10 09:57:38 +0300

    Создал класс AlertPresenter

--- MovieQuiz/Presentation/AlertPresenter.swift
+++ MovieQuiz/Presentation/AlertPresenter.swift
@@ -1,5 +1,22 @@
 import Foundation
+import UIKit
 
-class AlertPresenter {
+class AlertPresenter: QuestionAlertDelegate {
     
+    // метод назначения делегата
+    func appointDelegate(vc: MovieQuizViewController) {
+        vc.delegate = self
+    }
+    
+    // метод показа Алерта
+    func showAlert(modelAlert: AlertModel,  vc: MovieQuizViewController) {
+            let alert = UIAlertController(
+                title: modelAlert.title,
+                message: modelAlert.message,
+                preferredStyle: .alert)
+            
+        let action = UIAlertAction(title: modelAlert.buttonText, style: .default) { _ in modelAlert.completion() }
+            alert.addAction(action)
+            vc.present(alert, animated: true, completion: nil)
+    }
 }
